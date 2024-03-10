b37f8265a292200e13d3db1778f86dbd91972349
Gabriel Matias <ggezcom991gh@gmail.com>
2024-03-09 21:24:36 -0300

    alterações

---
+++ index.js
@@ -0,0 +1,34 @@
+let EMAIL = "admin@admin.com"
+let SENHA = "123456"
+
+let btnDeEntrar = document.getElementById("botaodeentrar")
+let CampoDoemail = document.getElementById("email")
+let CampoDaSenha = document.getElementById("senha")
+
+btnDeEntrar.addEventListener("click", () =>{
+    let emailDigitado = CampoDoemail.value.toLowerCase()
+    let senhaDigitada = CampoDaSenha.value
+    /*
+    if(emailDigitado != EMAIL || senhaDigitada != SENHA){
+        alert("Email ou senha incorretos! Tente novamente")
+        return
+    }
+    */
+   Autenticar(emailDigitado, senhaDigitada)
+})
+
+function Autenticar(email, senha){
+    const URL = "http://localhost:3400/login"
+    fetch(URL, {
+        method: "POST",
+        headers: {"Content-Type": "application/json"},
+        body: JSON.stringify({email, senha})
+    }).then(response => response = response.json())
+    .then(response => {
+        console.log(response)
+        window.open("testeDeHome.html", "_self")
+    })
+    .catch(erro => {
+        console.log(erro)
+    })
+}
