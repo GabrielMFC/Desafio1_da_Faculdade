62e42399365ccd419d914daeef14f203303a30a3
Gabriel Matias <ggezcom991gh@gmail.com>
2024-03-17 15:18:32 -0300

    Adicionando a função de editar produto

--- home.js
+++ home.js
@@ -1,31 +1,40 @@
-let tokenDeAcesso = localStorage.getItem("token");
-const url = "http://localhost:3400/produtos";
-let nome = document.getElementsByClassName("nome");
-let preco = document.getElementsByClassName("preco");
-let qtd = document.getElementsByClassName("qtd-estoque");
-let dataDeCadastro = document.getElementsByClassName("data");
-let bodyDaLista = document.getElementById("bodyDalista");
+let tokenDeAcesso = localStorage.getItem("token")
+const url = "http://localhost:3400/produtos"
+let nome = document.getElementsByClassName("nome")
+let preco = document.getElementsByClassName("preco")
+let qtd = document.getElementsByClassName("qtd-estoque")
+let dataDeCadastro = document.getElementsByClassName("data")
+let bodyDaLista = document.getElementById("bodyDalista")
 
-let btn_de_add = document.getElementById("adicionar");
-let btn_de_remover = document.getElementById("remover");
+let btn_de_add = document.getElementById("adicionar")
+let btn_de_remover = document.getElementById("remover")
+let btn_de_editar = document.getElementById("editar")
 
-let pagina_de_cadastro = document.getElementById("idPaginaCadastro");
-let btn_de_confirmar = document.getElementById("botaoDeConfirmarDoADD");
-let inputDeNome = document.getElementById("nome_input");
-let inputDevalor = document.getElementById("valor_input");
-let inputDeQtd = document.getElementById("qtd_em_estoque_input");
+let pagina_de_cadastro = document.getElementById("idPaginaCadastro")
+let btn_de_confirmar = document.getElementById("botaoDeConfirmarDoADD")
+let inputDeNome = document.getElementById("nome_input")
+let inputDevalor = document.getElementById("valor_input")
+let inputDeQtd = document.getElementById("qtd_em_estoque_input")
 
-let pagina_de_deletar = document.getElementById("idPaginaDelete");
-let btn_de_DELETAR = document.getElementById("botaoDeConfirmarDoREMOVER");
-let input_de_deletar = document.getElementById("delete_input");
+let pagina_de_deletar = document.getElementById("idPaginaDelete")
+let btn_de_DELETAR = document.getElementById("botaoDeConfirmarDoREMOVER")
+let input_de_deletar = document.getElementById("delete_input")
 
-function VerificarnumeroDeObjetos() {
+let pagina_de_editar = document.getElementById("idPaginaEditar")
+let btn_de_redefinir = document.getElementById("botaoDeRedefinir")
+let input_do_numero = document.getElementById("numeroParaEditar")
+let input_do_nome_editar = document.getElementById("input_de_nomeEditar")
+let input_do_valor_editar = document.getElementById("input_de_valorEditar")
+let input_da_qtd_editar = document.getElementById("input_de_qtdEditar")
+
+/* Verificar/Deletar produtos */
+function VerificarnumeroDeObjetosEdeletar() {
   fetch(url, {
     method: "GET",
     headers: {
       "Content-Type": "application/json",
-      Authorization: tokenDeAcesso,
-    },
+      Authorization: tokenDeAcesso
+    }
   })
     .then((Resposta) => Resposta.json())
     .then((Resposta) => {
@@ -47,15 +56,16 @@ function VerificarnumeroDeObjetos() {
         tdpreco.innerHTML = Resposta[i].valor
         tdqtd.innerHTML = Resposta[i].quantidadeEstoque
         tddata.innerHTML = Resposta[i].dataCadastro
-        console.log(Resposta)
       }
+      
       btn_de_remover.addEventListener("click", () => {
+        
         fetch("http://localhost:3400/produtos", {
           method: "GET",
           headers: {
             "Content-Type": "application/json",
             Authorization: tokenDeAcesso,
-          },
+          }
         })
           .then((Resposta) => Resposta.json())
           .then(() => {
@@ -63,6 +73,7 @@ function VerificarnumeroDeObjetos() {
           })
           .then(() => {
             btn_de_DELETAR.addEventListener("click", () => {
+              
               const itemAserDeletado = Resposta[input_de_deletar.value - 1]
               const urlParadeletar =
                 "http://localhost:3400/produtos"+ "/" + itemAserDeletado.id
@@ -73,11 +84,7 @@ function VerificarnumeroDeObjetos() {
                   Authorization: tokenDeAcesso,
                 }
               }).then(() => {
-                Resposta.id = Resposta.id - 1
-              })
-              .then(() => {
                 window.location.reload()
-                console.log(Resposta)
               })
             })
           })
@@ -86,10 +93,16 @@ function VerificarnumeroDeObjetos() {
     })
 }
 
+/* Adicionar produtos */
 btn_de_add.addEventListener("click", () => {
   pagina_de_cadastro.classList.remove("Desaparecer");
 });
 btn_de_confirmar.addEventListener("click", () => {
+  if(!inputDeNome.value || !inputDevalor.value || !inputDeQtd.value){
+    alert("Preencha todos os campos")
+    pagina_de_cadastro.classList.add("Desaparecer")
+    return
+  }
   fetch(url, {
     method: "GET",
     headers: {
@@ -97,55 +110,73 @@ btn_de_confirmar.addEventListener("click", () => {
       Authorization: tokenDeAcesso,
     },
   })
-    .then((response) => response.json())
-    .then((response) => {
-      let novoObjeto = {
-        id: response.length,
-        nome: inputDeNome.value,
-        valor: inputDevalor.value,
-        quantidadeEstoque: inputDeQtd.value,
-        observacao: "objeto novo",
-        dataCadastro: new Date()
-      }
-      fetch("http://localhost:3400/produtos", {
-          method: "POST",
-          headers: {
-            "Content-Type": "application/json",
-            Authorization: tokenDeAcesso
-          },
-          body: JSON.stringify(novoObjeto)
-        })
-        .then(() => {
-          window.location.reload()
-        })
+    .then((response) =>  {
+      response = response.json()
+        let novoObjeto = {
+          id: response.length,
+          nome: inputDeNome.value,
+          valor: inputDevalor.value,
+          quantidadeEstoque: inputDeQtd.value,
+          observacao: "objeto novo",
+          dataCadastro: new Date()
+        }
+        
+        fetch("http://localhost:3400/produtos", {
+            method: "POST",
+            headers: {
+              "Content-Type": "application/json",
+              Authorization: tokenDeAcesso
+            },
+            body: JSON.stringify(novoObjeto)
+          })
+          .then(() => {
+            window.location.reload()
+          })
+
     })
-    
-    
 })
 
-VerificarnumeroDeObjetos()
+/* Editar de produtos */
+btn_de_editar.addEventListener("click", () => {
+  pagina_de_editar.classList.remove("Desaparecer")
+  fetch(url, {
+    method: "GET",
+    headers: {
+      "Content-Type": "application/json",
+      Authorization: tokenDeAcesso
+    }
+  })
+  .then((Resposta) => Resposta.json())
+  .then((Resposta) => {
+    btn_de_redefinir.addEventListener("click", () => {
+      if(!input_do_numero.value){
+        alert("Digite o número do produto")
+        return
+      }
+    var ValoresEditados = {
+      nome: input_do_nome_editar.value ? input_do_nome_editar.value : Resposta[input_do_numero.value -1].nome,
+      valor: input_do_valor_editar.value ? input_do_valor_editar.value : Resposta[input_do_numero.value -1].valor,
+      quantidadeEstoque: input_da_qtd_editar.value ? input_da_qtd_editar.value : Resposta[input_do_numero.value -1].quantidadeEstoque,
+      dataCadastro: Resposta[input_do_numero.value -1].dataCadastro
+    }
 
+    if(!input_do_nome_editar.value && !input_do_valor_editar.value && !input_da_qtd_editar.value) {
+      alert("preencha pelo menos o campo de valor ou o de quantidade")
+      return
+    }
+    const itemAserEditado = Resposta[input_do_numero.value - 1]
+    fetch("http://localhost:3400/produtos" + "/" + itemAserEditado.id, {
+      method: "PUT",
+      headers: {
+        "Content-Type": "application/json",
+        Authorization: tokenDeAcesso,
+      },
+      body: JSON.stringify(ValoresEditados)
+    })
+    .then(() => {
+       window.location.reload()
+    })
+  })})
+})
 
-      // btn_de_remover.addEventListener("click", () => {
-      //       fetch("http://localhost:3400/produtos", {
-      //         method: "GET",
-      //         headers: {
-      //           "Content-Type": "application/json",
-      //           Authorization: tokenDeAcesso
-      //         },
-      //       })
-      //       .then((Resposta) => Resposta.json())
-      //       .then((Resposta) => {
-      //         const ultimoProduto = Resposta[Resposta.length - 1]
-      //         const URL3 = "http://localhost:3400/produtos" + "/" + ultimoProduto.id;
-      //         fetch(URL3, {
-      //           method: "DELETE",
-      //           headers: {
-      //             "Content-Type": "application/json",
-      //             Authorization: tokenDeAcesso},
-      //         }).then(() => {
-      //           window.location.reload()
-      //         })
-      //       })
-      //     })
-      
+VerificarnumeroDeObjetosEdeletar()
